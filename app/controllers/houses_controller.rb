bba23c503fdb4a495eda6221d642e266dd02beaf
Danielle Fricke <danielle.fricke11@gmail.com>
2017-06-08 10:55:31 -0400

    house add edit delete working

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -13,6 +13,15 @@ class HousesController < ApplicationController
           redirect_to "/houses/"
   end
 
+  def edit
+      @house = House.find(params[:id])
+    end
+
+    def update
+          @house = House.find(params[:id])
+          @house.update(house_params)
+          redirect_to house_path(@house)
+        end
   def show
     @house = House.find(params[:id])
   end
